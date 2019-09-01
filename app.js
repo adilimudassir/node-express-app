4a36f4107261c29399001e01612ba4a738e1e7b7
adilimudassir <mudassiradili4u@gmail.com>
2019-09-01 22:31:19 +0100

    add debugging tools

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(
+    morgan('combined')
+);
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
 app.listen(3000, () => {
-    console.log("Listening on port 3000")
+    debug(chalk(`Listening on port ${chalk.green(3000)}`))
 })
