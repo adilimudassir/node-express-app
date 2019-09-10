5ecc53c71f245e64afe5c8e7b2b53c88a7a73516
adilimudassir <mudassiradili4u@gmail.com>
2019-09-10 15:28:10 +0100

    add static files

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
-app.use(
-    morgan('combined')
-);
+const port = process.env.PORT || 3000;
+
+const staticPath = (filePath) => express.static(path.join(__dirname, filePath));
+
+app.use(morgan('combined'));
+app.use(staticPath('/public'));
+app.use('/css', staticPath('/node_modules/bootstrap/dist/css'));
+app.use('/js', staticPath('/node_modules/bootstrap/dist/js'));
+app.use('/js', staticPath('/node_modules/jquery/dist/'));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.render('index', {
+    list: ['a', 'b'],
+    title: 'Node Express App'
+  });
 });
 
-app.listen(3000, () => {
-    debug(chalk(`Listening on port ${chalk.green(3000)}`))
-})
+app.listen(port, () => {
+  debug(chalk(`Listening on port ${chalk.green(port)}`));
+});
